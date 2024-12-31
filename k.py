bd3120c2bcfe981c4a708d40699f65209f396b96
ronii432 <192245640+ronii432@users.noreply.github.com>
2024-12-31 23:51:11 +0530

    Update and rename k.py to soul.py

--- k.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-def find_free_port():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind(('', 0))  # Bind to an arbitrary free port
-        return s.getsockname()[1]
-
-free_port = find_free_port()
-print(f"Available free port: {free_port}")
